a53667d1184ca352460c9c66e55c6276de1a4736
rasulabduvaitov@gmail.com <rasulabduvaitov@gmail.com>
2023-07-23 18:20:36 +0500

    gin CRUD

---
+++ main.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+type Album struct {
+	ID     string  `json:"id"`
+	Title  string  `json:"title"`
+	Artist string  `json:"artist"`
+	Price  float64 `json:"price"`
+}
+
+type Error struct {
+	Error string `json:"error"`
+}
+
+var albums = []Album{
+	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
+	{ID: "2", Title: "Jeu", Artist: "Gerry Mulligan", Price: 17.99},
+	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/albums", getAlbums)
+	router.POST("/albums", postAlbum)
+	router.PUT("/albums/:id", updateAlbum)
+	router.GET("/albums/:id", getAlbumById)
+	router.DELETE("/albums/:id", deleteAlbum)
+
+	router.Run("localhost:8080")
+}
+
+func getAlbums(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, albums)
+}
+
+func postAlbum(c *gin.Context) {
+	var newAlbum Album
+	if err := c.BindJSON(&newAlbum); err != nil {
+		c.IndentedJSON(http.StatusBadRequest, Error{"bad request"})
+		return
+	}
+	albums = append(albums, newAlbum)
+	c.IndentedJSON(http.StatusCreated, newAlbum)
+}
+
+func getAlbumById(c *gin.Context) {
+	id := c.Param("id")
+
+	for _, a := range albums {
+		if a.ID == id {
+			c.IndentedJSON(http.StatusOK, a)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, Error{"Not Found"})
+}
+
+func deleteAlbum(c *gin.Context) {
+	id := c.Param("id")
+
+	for i, a := range albums {
+		if a.ID == id {
+			albums = append(albums[:i], albums[i+1:]...)
+			c.IndentedJSON(http.StatusNoContent, a)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, Error{"Not Found"})
+}
+
+func updateAlbum(c *gin.Context) {
+	id := c.Param("id")
+
+	for i, a := range albums {
+		if a.ID == id {
+			c.BindJSON(&a)
+			albums[i] = a
+			c.IndentedJSON(http.StatusOK, a)
+		}
+	}
+}
